e1b42111a31389fe0d16316e00b13edbc8c276dd
teddykarisa <teddykarisachaitkc@gmail.com>
2022-09-11 08:07:44 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main-print alphabets
+ *
+ * Return: Always 0(SUCCESS)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
