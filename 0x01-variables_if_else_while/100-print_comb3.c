f807efdbc75cb3cd3faaa9424c0b472267f0ff41
teddykarisa <teddykarisachaitkc@gmail.com>
2022-09-12 12:25:31 -0700

    combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
-
-
-
 /**
-
  * main - Prints numbers between 00 to 89.
-
- *
-
  * Return: Always 0 (Success)
-
  */
-
-
-
 int main(void)
-
 {
 
 	int i, e;
-
-
-
 	i = 48;
 
 	e = 48;
 
-
-
 	while (e < 58)
 
 	{
-
 		i = 48;
 
 		while (i < 58)
