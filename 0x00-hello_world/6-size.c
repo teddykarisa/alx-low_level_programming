6be69e43bec0c6effb8e2616fb39f80619288d42
teddykarisa <teddykarisachaitkc@gmail.com>
2022-09-12 05:20:47 -0700

    last

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,14 +10,8 @@ int main(void)
 
 {
 
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-
-printf("Size of a char is: %d byte(s)\n", sizeof(char);
-printf("Size of an int is: %d byte(s)\n", sizeof(int);
+printf("Size of a char is: %d byte(s)\n", sizeof(char));
+printf("Size of an int is: %d byte(s)\n", sizeof(int));
 printf("Size of a long int is: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int is: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float is: %d byte(s)\n", sizeof(float));
