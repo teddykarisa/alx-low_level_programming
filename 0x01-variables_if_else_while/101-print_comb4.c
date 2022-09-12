e53b9e419ae8beb6c2553e2a58cec3877fbb4134
teddykarisa <teddykarisachaitkc@gmail.com>
2022-09-12 12:51:16 -0700

    combo4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,36 +17,26 @@ int main(void)
 
 	while (e < 58)
 	{
-
 		i = 48;
 
 		while (i < 58)
 		{
-
 			g = 48;
 
 			while (g < 58)
 			{
-
-
-
 				if (e != i && e != g && i != g && e < i && i < g)
 				{
-
 					putchar(e);
-
 					putchar(i);
-
 					putchar(g);
 
 					if (i == 56 && e == 55 && g == 57)
 					{
-
 						break;
-
+						
 					}
 					putchar(',');
-
 					putchar(' ');
 
 				}
