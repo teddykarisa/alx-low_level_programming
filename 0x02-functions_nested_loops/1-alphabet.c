c4727d1c46320b5592fdfbfa3ce9c32766ea20a8
teddykarisa <teddykarisachaitkc@gmail.com>
2022-09-14 00:12:24 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets in lower case
+ *
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
