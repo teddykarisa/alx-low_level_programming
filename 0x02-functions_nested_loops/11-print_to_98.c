31f36bfbb88f6a79aa91caa3db970761e437334b
teddykarisa <teddykarisachaitkc@gmail.com>
2022-09-14 12:50:20 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,53 @@
 #include<stdio.h>
 
+
+
 /**
+
  * print_to_98 - last number is 98
+
  *
- * @n is integer
+
+ * @n: input  integer
+
  *
+
  * Return: 0 (success)
+
  */
- void print_to_98(int n)
+
+void print_to_98(int n)
+
 {
+
 	if (n < 98)
+
 	{
+
 		for (; n < 98; n++)
+
 		{
+
 			printf("%d, ", n);
+
 		}
+
 	}
+
 	else if (n > 98)
+
 	{
+
 		for (; n > 98; n--)
+
 		{
+
 			printf("%d, ", n);
+
 		}
+
 	}
+
 	printf("%d\n", n);
+
 }
