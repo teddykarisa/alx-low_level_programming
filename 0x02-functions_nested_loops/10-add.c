5ba6a44c2c6cabc9485cde54af25471f97f0307e
teddykarisa <teddykarisachaitkc@gmail.com>
2022-09-14 12:40:29 -0700

    adds

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,91 @@
 #include "main.h"
-/**
- * add - add two integers
- *
- *  @a: number one.
- *  @b: number two.
- *
- * Return: sum (success)
- */
-int add(int a, int b)
-{
-	int sum;
-	sum = a + b;
 
-	return (sum);
+
+
+/**
+
+ * print_times_table - Prints the n times table
+
+ *
+
+ * @n: number times table (0 < n <= 15)
+
+ *
+
+ * Return: no return
+
+ */
+
+void print_times_table(int n)
+
+{
+
+	int a, b, op;
+
+
+
+	if (n >= 0 && n <= 15)
+
+	{
+
+		for (a = 0; a <= n; a++)
+
+		{
+
+			_putchar(48);
+
+			for (b = 1; b <= n; b++)
+
+			{
+
+				op = a * b;
+
+				_putchar(44);
+
+				_putchar(32);
+
+				if (op <= 9)
+
+				{
+
+					_putchar(32);
+
+					_putchar(32);
+
+					_putchar(op + 48);
+
+				}
+
+				else if (op <= 99)
+
+				{
+
+					_putchar(32);
+
+					_putchar((op / 10) + 48);
+
+					_putchar((op % 10) + 48);
+
+				}
+
+				else
+
+				{
+
+					_putchar(((op / 100) % 10) + 48);
+
+					_putchar(((op / 10) % 10) + 48);
+
+					_putchar((op % 10) + 48);
+
+				}
+
+			}
+
+			_putchar('\n');
+
+		}
+
+	}
+
 }
