91bcf6f7ba1633402223ebcd99c63814d261bc3c
teddykarisa <teddykarisachaitkc@gmail.com>
2022-09-15 11:40:48 -0700

    mny

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints if integer is positive or negative
+ *
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
