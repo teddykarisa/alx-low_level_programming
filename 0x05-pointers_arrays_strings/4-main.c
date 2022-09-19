707ee9335d323d25b0878ca0a1443f3d23f5317c
teddykarisa <teddykarisachaitkc@gmail.com>
2022-09-19 05:42:43 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - checks code
+ * Return: 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
