91e654656cd021a538a83d72782747ae7ac63356
teddykarisa <teddykarisachaitkc@gmail.com>
2022-09-29 00:15:06 -0700

    task 2

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+
+ * main - checks code
+
+ *
+
+ * Return: 0
+
+ */
+
+
+
+int main(void)
+
+{
+
+	int n;
+
+
+
+	n = _strlen_recursion("Corbin Coleman");
+
+	printf("%d\n", n);
+
+	return (0);
+
+}
